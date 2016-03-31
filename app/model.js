c6443e3053ea5d7e48739a5fec6f96e818f8b787
AntonVolyk <volik.anton@gmail.com>
2016-03-31 14:04:17 +0300

    delete the garbage

--- app/model.js
+++
@@ -1,7 +0,0 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandler = require("./requestHandler");
-var handle = {};
-
-handle["/cinema.json"] = requestHandler.showInfo;
-server.start(router.route, handle);
